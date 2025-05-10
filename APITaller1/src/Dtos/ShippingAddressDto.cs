00292adca68e009c2680773697380ab58407402f
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2025-05-10 13:15:13 -0400

    feat: add ShippingAddressDto

---
+++ APITaller1/src/Dtos/ShippingAddressDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APITaller1.src.Dtos
+{
+    public class ShippingAddressDto
+    {
+        public int AddressID { get; set; }
+        public required string Street { get; set; }
+        public required string Number { get; set; }
+        public required string Commune { get; set; }
+        public required string Region { get; set; }
+        public required string PostalCode { get; set; }
+    }
+}
