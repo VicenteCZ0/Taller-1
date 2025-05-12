eb9f72bdf2dfa0eb73b08ad792378084570a8775
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2025-05-12 18:11:44 -0400

    feat: add new function in ProductRepository and new methods in IProductRepository

--- APITaller1/src/interfaces/IProductRepository.cs
+++ APITaller1/src/interfaces/IProductRepository.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
 using APITaller1.src.models;
 
-namespace APITaller1.src.interfaces;
-
-public interface IProductRepository
+namespace APITaller1.src.interfaces
 {
-    Task<Product> GetProductByIdAsync(int id);
-    Task<IEnumerable<Product>> GetProductsAsync();
-    Task AddProductAsync(Product product);
-    void DeleteProductAsync(Product product);
-    Task UpdateProductAsync(Product product);
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task AddProductAsync(Product product);
+        Task DeleteProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+
+        // Métodos adicionales opcionales:
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string category);
+        Task<IEnumerable<Product>> SearchProductsByNameAsync(string keyword);
+    }
 }
