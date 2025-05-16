0f24f5339ec24109609a5407798ba20805d4a04c
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2025-05-16 02:32:42 -0400

    feat: add Dto, repository, mapper and interface for product image

---
+++ APITaller1/src/Mappers/ProductImageMapper.cs
@@ -0,0 +1,33 @@
+using APITaller1.src.Dtos;
+using APITaller1.src.models;
+
+namespace APITaller1.src.Mappers
+{
+    public static class ProductImageMapper
+    {
+        public static ProductImageDto ToDto(ProductImage image)
+        {
+            return new ProductImageDto
+            {
+                ImageID = image.ImageID,
+                Url_Image = image.Url_Image,
+                ProductID = image.ProductID
+            };
+        }
+
+        public static ProductImage ToModel(CreateProductImageDto dto)
+        {
+            return new ProductImage
+            {
+                Url_Image = dto.Url_Image,
+                ProductID = dto.ProductID
+            };
+        }
+
+        public static void UpdateModel(ProductImage image, ProductImageDto dto)
+        {
+            image.Url_Image = dto.Url_Image;
+            image.ProductID = dto.ProductID;
+        }
+    }
+}
