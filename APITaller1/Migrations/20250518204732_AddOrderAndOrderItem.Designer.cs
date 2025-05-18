cb3f95388c7fbf6f8afe99c145819f14047f5205
VicenteCZ0 <vicente_castro_zepeda@hotmail.com>
2025-05-18 16:48:35 -0400

    feat: add new migrations

--- APITaller1/Migrations/20250518204732_AddOrderAndOrderItem.Designer.cs
+++ APITaller1/Migrations/20250518204732_AddOrderAndOrderItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using APITaller1.src.data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APITaller1.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20250518204732_AddOrderAndOrderItem")]
+    partial class AddOrderAndOrderItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.5");
